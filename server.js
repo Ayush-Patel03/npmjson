daf0778755c2c0724abc1f1670eaa9fa7e7ed581
Ayush Patel <ayushbp@email.sc.edu>
2023-11-02 12:32:48 -0400

    first

--- server.js
+++ server.js
@@ -1,5 +1,41 @@
 const express = require("express");
 const app = express();
+app.use(express.static("public"));
+
+app.get("/", (req, res) =>{
+    res.sendFile(__dirname + "/index.html");
+});
+ cc
+
+app.get("/api/animals", (req, res) => {
+    const animals = [];
+    animals[0] = {
+        name: "lion",
+        color: "yellow",
+        state: "fierce"
+    }
+    animals[1] = {
+        name: "dog",
+        color: "black",
+        state: "happy"
+    }
+    animals[2] = {
+        name: "bear",
+        color: "brown",
+        state: "big"
+    }
+    animals[3] = {
+        name: "penguin",
+        color: "black",
+        state: "sleep"
+    }
+    animals[4] = {
+        name: "cat",
+        color: "yellow",
+        state: "cati"
+    }
+    res.json(animals);
+});
 
 app.listen(3000, () => {
     console.log("listening");
